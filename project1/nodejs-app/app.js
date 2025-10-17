86e5d004d84cb196bc8be239e816dfea1fd19daa
Hema CJ <hemajairam@LAPTOP-3B2EDL9L.>
2025-10-17 16:46:53 +0000

    initial commit

---
+++ project1/nodejs-app/app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("Hello from Jenkins CI/CD!"));
+app.listen(8080, () => console.log("App running on port 8080"));
+
